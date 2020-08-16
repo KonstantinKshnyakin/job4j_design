590d5c3e194553d1a987d6ac8d8cea7fe6b08a26
K.Kshnyakin <kocmoc007@gmail.com>
2020-08-16 22:46:01 +0300

    5.2.2. Реализовать Store<T extends Base> [#341478]

--- chapter_001/src/main/java/ru/job4j/generic/MemStore.java
+++ chapter_001/src/main/java/ru/job4j/generic/MemStore.java
@@ -14,36 +14,44 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public boolean replace(String id, T model) {
-        for (int i = 0; i < mems.size(); i++) {
-            if (mems.get(i).getId().equals(id)) {
-                mems.set(i, model);
-                return true;
-            }
+        int index = privateFindById(id);
+        if (checkIndex(index)) {
+            return false;
         }
-        System.out.println("id не найден");
-        return false;
+        mems.set(index, model);
+        return true;
     }
 
     @Override
     public boolean delete(String id) {
-        for (T mem : mems) {
-            if (mem.getId().equals(id)) {
-                mems.remove(mem);
-                return true;
-            }
+        int index = privateFindById(id);
+        if (checkIndex(index)) {
+            return false;
         }
-        System.out.println("id не найден");
-        return false;
+        mems.remove(index);
+        return true;
     }
 
     @Override
     public T findById(String id) {
-        for (T mem : mems) {
-            if (mem.getId().equals(id)) {
-                return mem;
+        int index = privateFindById(id);
+        if (checkIndex(index)) {
+            return null;
+        }
+        return mems.get(index);
+    }
+
+    private int privateFindById(String id) {
+        for (int i = 0; i < mems.size(); i++) {
+            if (mems.get(i).getId().equals(id)) {
+                return i;
             }
         }
         System.out.println("id не найден");
-        return null;
+        return -1;
+    }
+
+    private boolean checkIndex(int index) {
+        return index == -1;
     }
 }
