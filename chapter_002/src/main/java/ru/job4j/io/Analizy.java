368cb1645c8116ceac8b457d99ca84fa6140580c
K.Kshnyakin <kocmoc007@gmail.com>
2020-08-27 19:01:06 +0300

    2. Анализ доступности сервера. [#341511]

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -7,46 +7,45 @@ import java.util.List;
 public class Analizy {
 
     public static void unavailable(String source, String target) {
-        List<String> read = read(source);
-        List<String> outputList = processing(read);
+        List<String> outputList = readAndProcessing(source);
         write(target, outputList);
     }
 
-    private static List<String> read(String source) {
-        List<String> lines = new ArrayList<>();
+    private static List<String> readAndProcessing(String source) {
+        final ArrayList<String> outputList = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            in.lines()
-                    .filter(s -> !s.isBlank() || !s.isEmpty())
-                    .forEach(lines::add);
+
+            StringBuilder builder = new StringBuilder();
+            String readLine = in.readLine();
+            boolean isStart = true;
+
+            while (readLine != null) {
+                if (!readLine.isBlank()) {
+                    String time = readLine.split(" ")[1];
+                    if (isStart && isNotAvailable(readLine)) {
+                        builder.append("с ").append(time).append(" до ");
+                        isStart = false;
+                    } else if (!isStart && isAvailable(readLine)) {
+                        builder.append(time).append(System.lineSeparator());
+                        outputList.add(builder.toString());
+                        builder = new StringBuilder();
+                        isStart = true;
+                    }
+                }
+               readLine = in.readLine();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return lines;
+        return outputList;
     }
 
-    private static List<String> processing(List<String> inputList) {
-        List<String> outputList = new ArrayList<>();
-        String status1 = "500";
-        String status2 = "400";
-        StringBuilder builder = new StringBuilder();
-        for (String str : inputList) {
-            String[] split = str.split(" ");
-            String element = split[0];
-            if (element.equals(status1) || element.equals(status2)) {
-                if (element.equals("500") || element.equals("400")) {
-                    builder.append("с ").append(split[1]).append(" до ");
-                    status1 = "200";
-                    status2 = "300";
-                } else {
-                    builder.append(split[1]).append("\r\n");
-                    outputList.add(builder.toString());
-                    builder = new StringBuilder();
-                    status1 = "500";
-                    status2 = "400";
-                }
-            }
-        }
-        return outputList;
+    private static boolean isNotAvailable(String line) {
+        return (line.startsWith("500") || line.startsWith("400"));
+    }
+
+    private static boolean isAvailable(String line) {
+        return (line.startsWith("200") || line.startsWith("300"));
     }
 
     private static void write(String target, List<String> outputList) {
