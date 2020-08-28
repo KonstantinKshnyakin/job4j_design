a7fe80d44ce1e06895754756bc40a4541ef6f587
K.Kshnyakin <kocmoc007@gmail.com>
2020-08-28 15:46:23 +0300

    1. Бот [#341527]

--- chapter_002/src/main/java/ru/job4j/socket/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/socket/EchoServer.java
@@ -5,6 +5,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
+    public static final String HELLO = "Hello";
+    public static final String EXIT = "Exit";
+    public static final String HELLO_FRIEND = "Hello, dear friend.";
+    public static final String OK_200 = "HTTP/1.1 200 OK";
+
     public static void main(String[] args) throws IOException {
         boolean isBye = false;
         try (ServerSocket server = new ServerSocket(9000)) {
@@ -14,16 +20,31 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str = in.readLine();
-                    while (!str.isEmpty()) {
-                        if (str.contains("/?msg=Bye")) {
-                            isBye = true;
+                    String responseStr = "";
+                    while (str != null && !str.isEmpty()) {
+                        if (str.contains("/?msg=")) {
+                            if (str.contains(EXIT)) {
+                                isBye = true;
+                                responseStr = EXIT;
+                            } else if (str.contains(HELLO)) {
+                                responseStr = HELLO_FRIEND;
+                            } else {
+                                responseStr = getResponseStr(str);
+                            }
                         }
                         System.out.println(str);
                         str = in.readLine();
                     }
-                    out.write(("HTTP/1.1 200 OK" + System.lineSeparator()).getBytes());
+                    final String sep = System.lineSeparator();
+                    out.write((OK_200 + sep + sep + responseStr).getBytes());
                 }
             }
         }
+    }
+
+    private static String getResponseStr(String requestStr) {
+        String[] splitReq = requestStr.split(" ");
+        String[] splitMsg = splitReq[1].split("=");
+        return splitMsg[1];
     }
 }
