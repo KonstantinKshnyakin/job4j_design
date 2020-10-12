70733e53089df42ef27a8d316e2b7b67d47cd834
K.Kshnyakin <kocmoc007@gmail.com>
2020-10-13 01:13:02 +0300

    0. CAS - операции [#382615]

--- chapter_007/src/main/java/ru/job4j/concurrent/CASCount.java
+++ chapter_007/src/main/java/ru/job4j/concurrent/CASCount.java
@@ -2,16 +2,12 @@ package ru.job4j.concurrent;
 
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @ThreadSafe
 public class CASCount {
 
-    private final AtomicReference<Integer> count = new AtomicReference<>();
-
-    public CASCount() {
-        count.set(0);
-    }
+    private final AtomicInteger count = new AtomicInteger();
 
     public void increment() {
         int exValue;
--- chapter_007/src/test/java/ru/job4j/concurrent/CASCountTest.java
+++ chapter_007/src/test/java/ru/job4j/concurrent/CASCountTest.java
@@ -12,14 +12,14 @@ public class CASCountTest {
         CASCount casCount = new CASCount();
         Thread thread1 = new Thread(
                 () -> {
-                    for (int i = 0; i < 50; i++) {
+                    for (int i = 0; i < 500; i++) {
                         casCount.increment();
                     }
                 }
         );
         Thread thread2 = new Thread(
                 () -> {
-                    for (int i = 0; i < 50; i++) {
+                    for (int i = 0; i < 500; i++) {
                         casCount.increment();
                     }
                 }
@@ -28,6 +28,6 @@ public class CASCountTest {
         thread2.start();
         thread1.join();
         thread2.join();
-        Assert.assertThat(casCount.get(), is(100));
+        Assert.assertThat(casCount.get(), is(1000));
     }
 }
